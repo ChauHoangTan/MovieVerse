e8a3fa0cbd3b03a8600b3134480f61572d44214d
ChauHoangTan <20127621@student.hcmus.edu.vn>
2023-09-17 01:33:36 +0700

    change setHeader

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://chauhoangtan.github.io');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000,https://chauhoangtan.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
